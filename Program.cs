1fd60c854763588d6d730e5cdd9bbd2f4d3681be
Erika Araújo Anjos de Lima Silva <104324251+erikaaraujo81@users.noreply.github.com>
2023-12-15 09:39:42 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Teste do nokia");
+Smartphone nokia = new Nokia(numero: "92222-2222", modelo: "Acabo", imei: "000000000", memoria: 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("TikTok");
+
+
+Console.WriteLine("Teste do Iphone");
+Smartphone iphone = new Iphone(numero: "91111-1111", modelo: "14", imei: "111111111", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
+
+// TODO: Realizado os testes com as classes Nokia e Iphone!!!
